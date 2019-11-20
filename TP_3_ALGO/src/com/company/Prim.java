865703a47a90d91f61d8560e8c04e474191e0c40
valentinflegon <43956710+valentinflegon@users.noreply.github.com>
2019-11-20 17:41:13 +0100

    Update Prim.java

--- TP_3_ALGO/src/com/company/Prim.java
+++ TP_3_ALGO/src/com/company/Prim.java
@@ -1,22 +1,69 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Prim {
-    // verifier si le graph est connexe
 
-    //partir d'un noeud
+    public Prim() {
+    }
 
-    //choisir son path de poid le plus faible
+// verifier si le graph est connexe
 
-    //relier a sont noeud de fin
+    public void prim(Graph graph){
 
-    // attention a comment on choisi les aretes
-    //c'est le min des path de tout les noeuds en contact
-    // tant que tout les noeuds ne sont pas tout utiliser
+        //partir d'un noeud
+        //ici le premier noeud rentré
+        Node initNode = graph.arrayListOfNode.get(0);
+        graph.arrayListOfUseNode.add(initNode);
 
-    // ATTENTION a pas faire un triangle car il ne faut pas que les
-    //deux extremité du path soit deja dans l'arbre
+        do{
+            for (int i=0;i<graph.arrayListOfUseNode.size();i++){
+                Path goodPath = findBestPath(graph.arrayListOfUseNode);
+                graph.arrayListOfUseNode.add(goodPath.getEnd());
+            }
+
+        //on fait ca tant que tout les nodes ne sont pas utilisé
+        // cad il faut qu'il y ait autant de node utilisé que de Node rentré
+        }while (graph.arrayListOfNode.size() != graph.arrayListOfUseNode.size());
+    }
+
 
     // il faudra calculer le poids
+
+
+
+    //choisir son path de poid le plus faible
+    // pas utiliser la transition deja prise
+    public Path findBestPath(ArrayList<Node> arrayOfNode){
+
+        //initialisation
+        int weightTransitionMin = arrayOfNode.get(0).getArrayOfPath(0).getWeight();
+        Path pathMin = arrayOfNode.get(0).getArrayOfPath(0);
+
+        //on parcourt  tout les nodes de la liste
+        for(int i=0;i < arrayOfNode.size(); i++){
+
+            //on parcourt toutes les transitions de chaque nodes
+            for (int j=0; j < arrayOfNode.get(i).getArrayOfPath().size();j++){
+
+                int transitionCurrent = arrayOfNode.get(i).getArrayOfPath(j).getWeight();
+                //on verifie que la transition n'est pas deja utilisé
+                if(arrayOfNode.get(i).getArrayOfPath(j).isUsed() == false){
+
+                    //on stock la transition de poids min
+                    if (transitionCurrent < weightTransitionMin){
+                        weightTransitionMin = transitionCurrent;
+                        pathMin = arrayOfNode.get(i).getArrayOfPath(j);
+                    }
+                }
+            }
+        }
+
+        //on met la variable a utilisé
+        pathMin.setVariableIsUsed(true);
+        return pathMin;
+
+    }
 
 
 
