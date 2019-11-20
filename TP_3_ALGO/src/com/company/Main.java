b7286973af63267b607e490080c875dfced0e00c
valentinflegon <43956710+valentinflegon@users.noreply.github.com>
2019-11-20 17:42:46 +0100

    Update Main.java

--- TP_3_ALGO/src/com/company/Main.java
+++ TP_3_ALGO/src/com/company/Main.java
@@ -3,13 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Node nodeA = new Node('a');
-        Node nodeB = new Node('b');
-        Path p1 = new Path(nodeA,nodeB,3);
-        p1.display();
-        Path p2 = new Path(nodeA,nodeB,99);
-        p2.display();
+        Node node1 = new Node('a');
+        Node node2 = new Node('b');
+        Graph graph = new Graph();
+        graph.addNode(node1);
+        Path path1 = new Path(node1,node2,10);
+        graph.addPath(path1);
+        for(int i=0;i<graph.arrayListOfNode.size();i++){
+            System.out.println(graph.arrayListOfNode.get(i).getLabel());
+            System.out.println(graph.arrayListOfNode.get(i).getArrayOfPath().get(0).getWeight());
+            System.out.println(graph.arrayListOfNode.get(i).getArrayOfPath().get(0).getEnd().getLabel());
 
+
+        }
     }
 
 }
