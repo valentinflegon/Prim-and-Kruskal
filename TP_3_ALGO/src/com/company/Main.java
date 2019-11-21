10eac280c731c987a2b061866d23229216e40376
valentinflegon <43956710+valentinflegon@users.noreply.github.com>
2019-11-21 21:18:40 +0100

    Update Main.java

--- TP_3_ALGO/src/com/company/Main.java
+++ TP_3_ALGO/src/com/company/Main.java
@@ -5,17 +5,40 @@ public class Main {
     public static void main(String[] args) {
         Node node1 = new Node('a');
         Node node2 = new Node('b');
+        Node node3 = new Node('c');
+        Node node4 = new Node('d');
+
         Graph graph = new Graph();
+
         graph.addNode(node1);
+        graph.addNode(node2);
+        graph.addNode(node3);
+        graph.addNode(node4);
+
         Path path1 = new Path(node1,node2,10);
+        Path path2 = new Path(node1,node3,1);
+        Path path = new Path(node2,node4,4);
+        Path path3 = new Path(node3,node2,2);
+
         graph.addPath(path1);
-        for(int i=0;i<graph.arrayListOfNode.size();i++){
-            System.out.println(graph.arrayListOfNode.get(i).getLabel());
-            System.out.println(graph.arrayListOfNode.get(i).getArrayOfPath().get(0).getWeight());
-            System.out.println(graph.arrayListOfNode.get(i).getArrayOfPath().get(0).getEnd().getLabel());
+        graph.addPath(path2);
+        graph.addPath(path);
+        graph.addPath(path3);
 
 
-        }
+
+       /* System.out.println(path2.isVariableIsUsed());
+        System.out.println(graph.findBestPath(graph.arrayListOfNode).getWeight());
+        System.out.println(path2.isVariableIsUsed());
+        System.out.println(graph.findBestPath(graph.arrayListOfNode).getWeight());
+        System.out.println("Poids des chemins = ");
+        System.out.println(graph.getSommePath());
+
+
+       Prim prim = new Prim();
+       System.out.println("Poids des chemins du graph "+prim.prim(graph));
+ */
+       graph.arrayListOfNodeWhereSortPath(graph.arrayListOfNode);
     }
 
 }
