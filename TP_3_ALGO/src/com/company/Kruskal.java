03411a074464249f62318e3a8ac7309c90b727c8
Matthieulele <45042711+Matthieulele@users.noreply.github.com>
2019-11-24 02:19:33 +0100

    Update Kruskal.java

--- TP_3_ALGO/src/com/company/Kruskal.java
+++ TP_3_ALGO/src/com/company/Kruskal.java
@@ -3,52 +3,57 @@ import java.util.ArrayList;
 import java.util.Collections;
 public class Kruskal {
     private int sommePath;
+    Graph graph;
 
-    public Kruskal() {
+    public Kruskal(Graph graph) {
         this.sommePath = 0;
+        this.graph = graph;
     }
 
     /**
      *
-     * @param graph
+     * @param
      * @return
      */
-    public int kruskal(Graph graph){
-
-
-
+    public int kruskal(){
         Path goodPath;
-
         do{
-            goodPath = findBestPath(graph.arrayListOfNode);
+            goodPath = findBestPath();
+            goodPath.getBegin().setUsed(true);
+            goodPath.getEnd().setUsed(true);
+            goodPath.setVariableIsUsed(true);
+            goodPath.getBegin().getArrayOfPath().get(goodPath.getBegin().getArrayOfPath().indexOf(goodPath)).setVariableIsUsed(true);
+            goodPath.getEnd().getArrayOfPath().get(goodPath.getEnd().getArrayOfPath().indexOf(goodPath)).setVariableIsUsed(true);
             graph.arrayListOfUseNode.add(goodPath.getBegin());
             graph.arrayListOfUseNode.add(goodPath.getEnd());
-
         }while (graph.arrayListOfNode.size() != graph.arrayListOfUseNode.size());
+        for (Node node: graph.arrayListOfUseNode
+             ) {
 
+        }
         return getSommePath();
     }
 
     /**
      * fonction qui va trier toutes les transitions
      *
-     * @param arrayListOfNode
+     * @param
      * @return une liste de transitions triées
      */
-    public ArrayList<Path> arrayListOfNodeWhereSortPath(ArrayList<Node> arrayListOfNode){
+    public ArrayList<Path> arrayListOfNodeWhereSortPath(){
         ArrayList<Path> listeTrieOfPath = new ArrayList<>();
 
         //on ajoute toute les transitions dans une liste
-        for (int i=0;i<arrayListOfNode.size();i++){
-            for (int j=0;j<arrayListOfNode.get(i).getArrayOfPath().size();j++){
-                listeTrieOfPath.add(arrayListOfNode.get(i).getArrayOfPath().get(j));
+        for (int i=0;i<graph.arrayListOfNode.size();i++){
+            for (int j=0;j<graph.arrayListOfNode.get(i).getArrayOfPath().size();j++){
+                listeTrieOfPath.add(graph.arrayListOfNode.get(i).getArrayOfPath().get(j));
                 System.out.println(listeTrieOfPath.get(i).getWeight());
             }
         }
 
         //on trie la liste
         //a voir
-        Collections.sort(listeTrieOfPath);
+       // Collections.sort(listeTrieOfPath);
 
         //retourne une liste avec toute les transitions trié
         return listeTrieOfPath;
@@ -56,14 +61,14 @@ public class Kruskal {
 
     /**
      *
-     * @param arrayOfNode
+     * @param
      * @return
      */
-    public Path findBestPath(ArrayList<Node> arrayOfNode) {
-        ArrayList<Path> arrayListSortPath = arrayListOfNodeWhereSortPath(arrayOfNode);
+    public Path findBestPath() {
+        ArrayList<Path> arrayListSortPath = arrayListOfNodeWhereSortPath();
         Path goodPath ;
         for (int i=0;i<arrayListSortPath.size();i++){
-            if (arrayListSortPath.get(i).isVariableIsUsed() != false ){
+            if (!arrayListSortPath.get(i).isVariableIsUsed()){
                 goodPath = arrayListSortPath.get(i);
                 arrayListSortPath.get(i).setVariableIsUsed(true);
                 sommePath = sommePath + goodPath.getWeight();
