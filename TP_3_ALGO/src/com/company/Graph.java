1f503b515c05dcdd6194517a96532b17123b9d8a
Matthieulele <45042711+Matthieulele@users.noreply.github.com>
2019-11-25 00:42:54 +0100

    Update Graph.java

--- TP_3_ALGO/src/com/company/Graph.java
+++ TP_3_ALGO/src/com/company/Graph.java
@@ -24,6 +24,7 @@ public class Graph extends JComponent {
 
     public void addNode(Node node){
         this.arrayListOfNode.add(node);
+        this.arrayListOfNode.get(this.arrayListOfNode.size()-1).setIndex(arrayListOfNode.size()-1);
         arrayListOfPoint.add(new Point(50,50));
     }
 
@@ -37,7 +38,9 @@ public class Graph extends JComponent {
         Painter painter = new Painter(800,600);
         int begin, end;
         ArrayList<Path> printedPath = new ArrayList<>();
-        Main.createPoints(points);
+        for (int i = 0; i<arrayListOfNode.size(); i++) {
+            points[i] = new Point(arrayListOfNode.get(i).getX(), arrayListOfNode.get(i).getY());
+        }
         Main.drawPoints(painter,points);
         for (Node node:arrayListOfUseNode) {
             for (Path path:node.getArrayOfPath()) {
@@ -45,34 +48,46 @@ public class Graph extends JComponent {
                     printedPath.add(path);
                     begin = arrayListOfUseNode.indexOf(path.getBegin());
                     end = arrayListOfUseNode.indexOf(path.getEnd());
-                    System.out.println(path.getBegin().getLabel() + "-" + path.getWeight() + "-" + path.getEnd().getLabel());
+                    //System.out.println(path.getBegin().getLabel() + "-" + path.getWeight() + "-" + path.getEnd().getLabel());
                     points[begin].drawLine(points[end],painter,Color.lightGray);
                 }
             }
         }
-
-        /*
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setPaint(new Color(10, 50, 50));
-        Shape circle = new Rectangle();
-        circle.contains(40,52);
-        g2d.draw(circle);*/
     }
+    public void display1(){
+        Point[] points = new Point[arrayListOfNode.size()];
+        Painter painter = new Painter(800,600);
+        int begin, end;
+        for (int i = 0; i<arrayListOfNode.size(); i++) {
+            points[i] = new Point(arrayListOfNode.get(i).getX(), arrayListOfNode.get(i).getY());
+        }
+        Main.drawPoints(painter,points);
+        for (Node node:arrayListOfNode) {
+            for (Path path:node.getArrayOfPath()) {
+                begin = arrayListOfNode.indexOf(path.getBegin());
+                end = arrayListOfNode.indexOf(path.getEnd());
+                //System.out.println(path.getBegin().getLabel() + "-" + path.getWeight() + "-" + path.getEnd().getLabel());
+                points[begin].drawLine(points[end],painter,Color.lightGray);
+            }
+        }
+        Main.drawPoints(painter,points);
+    }
+
 
     public void display2(){
         Point[] points = new Point[arrayListOfNode.size()];
         Painter painter = new Painter(800,600);
         int begin, end;
-
-        Main.createPoints(points);
+        for (int i = 0; i<arrayListOfNode.size(); i++) {
+            points[i] = new Point(arrayListOfNode.get(i).getX(), arrayListOfNode.get(i).getY());
+        }
         Main.drawPoints(painter,points);
         for (Node node:arrayListOfNode) {
             for (Path path:node.getArrayOfPath()) {
                 if(path.isUsed()) {
                     begin = arrayListOfNode.indexOf(path.getBegin());
                     end = arrayListOfNode.indexOf(path.getEnd());
-                    System.out.println(path.getBegin().getLabel() + "-" + path.getWeight() + "-" + path.getEnd().getLabel());
+                    //System.out.println(path.getBegin().getLabel() + "-" + path.getWeight() + "-" + path.getEnd().getLabel());
                     points[begin].drawLine(points[end],painter,Color.lightGray);
                 }
             }
