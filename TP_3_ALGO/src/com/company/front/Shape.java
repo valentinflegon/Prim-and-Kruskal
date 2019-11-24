144ac734f4e1218627f9de1bfb5e77a875daab6c
Matthieulele <45042711+Matthieulele@users.noreply.github.com>
2019-11-24 02:18:35 +0100

    Add files via upload

---
+++ TP_3_ALGO/src/com/company/front/Shape.java
@@ -0,0 +1,15 @@
+package com.company.front;
+
+import tp2.lib.Painter;
+
+import java.awt.*;
+
+/**
+ * Created by Arnaud Labourel on 15/10/2018.
+ */
+public interface Shape {
+    double getPerimeter();
+    void draw(Painter painter, Color color);
+    Shape translate(int dx, int dy);
+    double getArea();
+}
